6f13bafdcfb0916004a0283e10eb868b7c1061dd
Josh Comer <joshjcomer@gmail.com>
2012-04-06 09:55:19 -0300

    Added functions to break apart word

--- src/cljDCPU/core.clj
+++ src/cljDCPU/core.clj
@@ -1,5 +1,26 @@
 (ns cljDCPU.core)
 
+(defn- mask-and-shift
+  "Generates a function which applies a bit mask to a word and then
+   right shifts the result"
+  [mask shift]
+  (fn [word]
+    (-> word
+        (bit-and mask)
+        (bit-shift-right shift))))
+
+(def get-o
+  "retrieves the opcode from the word"
+  (mask-and-shift 0xF 0))
+
+(def get-a
+  "retrieves the a parameter from the word"
+  (mask-and-shift 0x3F0 4))
+
+(def get-b
+  "retrieves the b parameter from the word"
+  (mask-and-shift 0xFC00 10))
+
 (defn -main
   "I don't do a whole lot."
   [& args]
