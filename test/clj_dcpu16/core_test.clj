5627626ab4df0c89cd72aabb831e770b2109d8e8
Josh Comer <joshjcomer@gmail.com>
2012-04-08 15:23:43 -0300

    Added more tests, still lots to write

--- test/clj_dcpu16/core_test.clj
+++ test/clj_dcpu16/core_test.clj
@@ -2,6 +2,9 @@
   (:use clojure.test
         clj-dcpu16.core))
 
+(defn- clear-memory
+  (set-memory (fn [] {:pc 0x0000 :sp 0xFFFF})))
+
 (deftest word-parsing
   (testing "Word Parsing"
     (testing "Word 0x7C01"
@@ -14,3 +17,12 @@
         (is (= 1 (get-o word)))
         (is (= 0x1C (get-a word)))
         (is (= 0x18 (get-b word)))))))
+
+(deftest instruction-length
+  (testing "Instruction Length"
+    (testing "Word 0x7DE1"
+      (is (= 3 (op-size 0x7DE1))))
+    (testing "Word 0x7C10"
+      (is (= 2 (op-size 0x7C10))))
+    (testing "Word 0x9037"
+      (is (= 1 (op-size 0x9037))))))
