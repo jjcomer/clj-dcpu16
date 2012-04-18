bd4ab01ceee7762513204aee0aaa36337f5a7d5d
Josh Comer <joshjcomer@gmail.com>
2012-04-18 10:50:42 -0300

    Bumped to clojure 1.4.0

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/jjcomer/cljDCPU"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
